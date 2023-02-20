b625b567cd24130d0394834024553bcd7f1bb260
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-02-20 08:48:57 -0300

    Delete JAVASCRIPT directory

--- JAVASCRIPT/01/js3/js/externo.js
+++
@@ -1,7 +0,0 @@
-         alert ("é bom ter você aqui!, é bom mesmo"); //Mensagem para o usuário
-
-	//Variaveis
-	var valor01=0;
-	var valor02;
-	document.write("O valor default da variável valor01 era "+valor01+".<br>");
-
